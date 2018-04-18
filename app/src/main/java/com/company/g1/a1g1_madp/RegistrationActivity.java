af6704852c4975d06892bc2f191dfb3112dee1be
STommydx <ttoo74@gmail.com>
2018-04-18 19:15:39 +0800

    Camera intent setup

--- app/src/main/java/com/company/g1/a1g1_madp/RegistrationActivity.java
+++ app/src/main/java/com/company/g1/a1g1_madp/RegistrationActivity.java
@@ -1,11 +1,16 @@
 package com.company.g1.a1g1_madp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+	static final int REQUEST_IMAGE_CAPTURE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,23 @@ public class RegistrationActivity extends AppCompatActivity {
 		intent.putExtra("STAGE_STATE", StageActivity.STAGE_NEW);
 		startActivity(intent);
 		finish();
+	}
+
+	public void dispatchTakePictureIntent(View view) {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap) extras.get("data");
+			ImageView mImageView = findViewById(R.id.imageView);
+			mImageView.setImageBitmap(imageBitmap);
+		}
 	}
 
 }
