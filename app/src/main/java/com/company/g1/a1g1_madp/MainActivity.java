71d9a29e0831b9962eb330e40e99f2e690897d66
Sum_01 <35803102+jask110a@users.noreply.github.com>
2018-04-19 04:19:24 +0800

    Animation added

--- app/src/main/java/com/company/g1/a1g1_madp/MainActivity.java
+++ app/src/main/java/com/company/g1/a1g1_madp/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+	LinearLayout l1,l2;
+	Animation uptodown,downtoup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		l1 = (LinearLayout) findViewById(R.id.l1);
+		l2 = (LinearLayout) findViewById(R.id.l2);
+		uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+		downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+		l1.setAnimation(uptodown);
+		l2.setAnimation(downtoup);
 	}
 
 	public void startGame(View view) {
