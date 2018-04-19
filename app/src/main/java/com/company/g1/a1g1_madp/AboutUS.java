7115b076deb95b156a050ccbdd246c5c25024f33
goodmorning3 <pui395@yahoo.com.hk>
2018-04-20 00:55:14 +0800

    True 'nightly' build.
    Changelog:
     - Added about us activity
     - Logo inside about us is now animated
     - Buttons in MainActivity are now bigger,
       with background colour and coloured border
     - Changed background colour in MainActivity to fit
       the analogous color scheme with the colour of the buttons

--- app/src/main/java/com/company/g1/a1g1_madp/AboutUS.java
+++ app/src/main/java/com/company/g1/a1g1_madp/AboutUS.java
@@ -2,6 +2,12 @@ package com.company.g1.a1g1_madp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutUS extends AppCompatActivity {
 
@@ -9,5 +15,8 @@ public class AboutUS extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about_us);
+		ImageView tv = (ImageView)findViewById(R.id.logoview);
+		Animation swipe =	AnimationUtils.loadAnimation(this, R.anim.scroll_animation);
+		tv.startAnimation(swipe);
 	}
 }
