8d05911ceaed3c233582e71e6133a25e062f2124
STommydx <ttoo74@gmail.com>
2018-04-22 17:16:00 +0800

    Try to make main menu look nicer

--- app/src/main/java/com/company/g1/a1g1_madp/MainActivity.java
+++ app/src/main/java/com/company/g1/a1g1_madp/MainActivity.java
@@ -4,16 +4,16 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-	LinearLayout l1,l2;
-	Animation uptodown,downtoup;
 
 	private MediaPlayer mediaPlayer;
 	private boolean playing;
@@ -22,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		LinearLayout lin = (LinearLayout) findViewById(R.id.l0);
+		ConstraintLayout lin = findViewById(R.id.l0);
 		lin.setBackgroundColor(Color.parseColor("#FFA000"));
-		l1 = (LinearLayout) findViewById(R.id.l1);
-		l2 = (LinearLayout) findViewById(R.id.l2);
-		uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
-		downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+		ConstraintLayout l1 = findViewById(R.id.topPanel);
+		ImageView l2 = findViewById(R.id.imageView8);
+		Animation uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+		Animation downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
 		l1.setAnimation(uptodown);
 		l2.setAnimation(downtoup);
 
