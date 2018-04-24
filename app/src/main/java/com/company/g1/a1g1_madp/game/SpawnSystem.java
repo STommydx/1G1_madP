6715a5f3c867edd46b4697b042f1bfc8c90d4bb8
STommydx <ttoo74@gmail.com>
2018-04-24 19:05:44 +0800

    Fix #41

--- app/src/main/java/com/company/g1/a1g1_madp/game/SpawnSystem.java
+++ app/src/main/java/com/company/g1/a1g1_madp/game/SpawnSystem.java
@@ -1,7 +1,10 @@
 package com.company.g1.a1g1_madp.game;
 
 import android.os.Handler;
+import com.company.g1.a1g1_madp.game.entity.Bullet;
 import com.company.g1.a1g1_madp.game.entity.Enemy;
+import com.company.g1.a1g1_madp.game.entity.GameObject;
+import com.company.g1.a1g1_madp.game.entity.Spaceship;
 
 public class SpawnSystem {
 
@@ -14,14 +17,21 @@ public class SpawnSystem {
 		public void run() {
 			float x = (float) (Math.random() * layoutWidth * 0.9);
 			Enemy mEnemy = new Enemy(x, 0);
+			mEnemy.addOnHitListener(source -> {
+				if (source instanceof Spaceship || source instanceof Bullet) {
+					context.getShopSystem().setMoney(context.getShopSystem().getMoney() + mEnemy.getScore());
+				}
+			});
 			entityRegister.registerEnemy(mEnemy);
 			spawnHandler.postDelayed(this, ENEMY_SPAWN_RATE);
 		}
 	};
 
 	private EntityRegister entityRegister;
+	private Game context;
 
 	SpawnSystem(Game context) {
+		this.context = context;
 		entityRegister = context.getEntityRegister();
 		layoutWidth = context.getLayoutWidth();
 	}
