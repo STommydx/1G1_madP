7477a70ed75917de16a1c77d62f6dff19b78d386
STommydx <ttoo74@gmail.com>
2018-04-25 10:54:35 +0800

    Fix img not displaying

--- app/src/main/java/com/company/g1/a1g1_madp/Tutorial3.java
+++ app/src/main/java/com/company/g1/a1g1_madp/Tutorial3.java
@@ -1,14 +1,13 @@
 package com.company.g1.a1g1_madp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.ImageView;
 
-public class Tutorial3 extends Activity{
+public class Tutorial3 extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
